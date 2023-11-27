da5483df46ce3dcdbe5bbf172d9af42b2d3d098a
Jorge <jorgericoinformatic@gmail.com>
2023-11-27 22:15:23 +0100

    Added clone to features

--- src/indexed_enum/mod.rs
+++ src/indexed_enum/mod.rs
@@ -1,23 +1,23 @@
 pub trait Indexed: Sized + 'static {
     const VARIANTS: &'static [Self];
 
-    fn discriminant(&self) -> usize {
+    fn index(&self) -> usize {
         unsafe { *<*const _>::from(self).cast::<usize>() }
     }
 
-    fn split_discriminants_to_offsets(discriminant: usize) -> (isize, isize) {
-        match TryInto::<isize>::try_into(discriminant) {
-            Ok(discriminant) => { (discriminant, 0) }
-            Err(_) => { (isize::MAX, (discriminant - (isize::MAX as usize)) as isize) }
+    fn split_index_to_offsets(index: usize) -> (isize, isize) {
+        match TryInto::<isize>::try_into(index) {
+            Ok(index) => { (index, 0) }
+            Err(_) => { (isize::MAX, (index - (isize::MAX as usize)) as isize) }
         }
     }
 
-    fn from_discriminant_opt(discriminant: usize) -> Option<Self> {
-        let (first_offset, second_offset) = Self::split_discriminants_to_offsets(discriminant);
+    fn from_index_opt(index: usize) -> Option<Self> {
+        let (first_offset, second_offset) = Self::split_index_to_offsets(index);
         Some(unsafe { Self::VARIANTS.as_ptr().offset(first_offset).offset(second_offset).read() })
     }
 
-    fn from_discriminant(discriminant: usize) -> Self {
-        Self::from_discriminant_opt(discriminant).unwrap()
+    fn from_index(index: usize) -> Self {
+        Self::from_index_opt(index).unwrap()
     }
 }
