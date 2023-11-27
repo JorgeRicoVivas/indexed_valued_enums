f6d8a8457e4340ea7243d394042f342a95d17d5b
Jorge <jorgericoinformatic@gmail.com>
2023-11-27 17:58:11 +0100

    The 'create_indexed_valued_enum' macro now doesn't ask for imports

--- src/macros.rs
+++ src/macros.rs
@@ -1,10 +1,10 @@
 #[macro_export]
-macro_rules! indexed_and_valued_enum {
+macro_rules! create_indexed_valued_enum {
     (process features
         [$enum_name:ident, $value_type:ty],
         [DerefToValue $($other_features:tt)*]
     )=>{
-        impl Deref for $enum_name{
+        impl core::ops::Deref for $enum_name{
             type Target = $value_type;
 
             fn deref(&self) -> &Self::Target {
@@ -12,7 +12,7 @@ macro_rules! indexed_and_valued_enum {
             }
         }
 
-        indexed_and_valued_enum !{process features [$enum_name, $value_type], [$($other_features)*]}
+        create_indexed_valued_enum !{process features [$enum_name, $value_type], [$($other_features)*]}
     };
     (process features
         [$enum_name:ident, $value_type:ty],
@@ -24,7 +24,7 @@ macro_rules! indexed_and_valued_enum {
             }
         }
 
-        indexed_and_valued_enum !{process features [$enum_name, $value_type], [$($other_features)*]}
+        create_indexed_valued_enum !{process features [$enum_name, $value_type], [$($other_features)*]}
     };
     (process features
         [$enum_name:ident, $value_type:ty],
@@ -32,7 +32,7 @@ macro_rules! indexed_and_valued_enum {
     )=>{
         impl<'de> serde::Deserialize<'de> for $enum_name {
             fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: serde::Deserializer<'de> {
-                match deserializer.deserialize_u128(DISCRIMINANT_VISITOR) {
+                match deserializer.deserialize_u128(indexed_valued_enums::serde_compatibility::discriminant_visitor::DISCRIMINANT_VISITOR) {
                     Ok(value) => {
                         match $enum_name::from_index_opt(value) {
                             Some(value) => { Ok(value) }
@@ -45,7 +45,7 @@ macro_rules! indexed_and_valued_enum {
         }
 
 
-        indexed_and_valued_enum !{process features [$enum_name, $value_type], [$($other_features)*]}
+        create_indexed_valued_enum !{process features [$enum_name, $value_type], [$($other_features)*]}
     };
 
     (process features [$enum_name:ident, $value_type:ty], [])=>{};
@@ -63,15 +63,15 @@ macro_rules! indexed_and_valued_enum {
             $($variants),+
         }
 
-        impl Indexed for $enum_name {
+        impl indexed_valued_enums::valued_enum::Indexed for $enum_name {
             const VARIANTS: &'static [ Self ] = &[$($enum_name::$variants),+];
         }
 
-        impl Valued for $enum_name {
+        impl indexed_valued_enums::valued_enum::Valued; for $enum_name {
             type Value = $value_type;
             const VALUES: &'static [ Self::Value] = & [$($values),+];
         }
 
-        indexed_and_valued_enum !{process features [$enum_name, $value_type], [$($features)*] }
+        create_indexed_valued_enum !{process features [$enum_name, $value_type], [$($features)*] }
     };
 }
