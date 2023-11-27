00df06644fc6d835f1e909854780eb75c1d03a6f
Jorge <jorgericoinformatic@gmail.com>
2023-11-27 22:17:30 +0100

    Added clone to features

--- src/valued_enum/mod.rs
+++ src/valued_enum/mod.rs
@@ -6,7 +6,7 @@ pub trait Valued: Indexed {
     const VALUES: &'static [Self::Value];
 
     fn value_opt(&self) -> Option<Self::Value> {
-        let (first_offset, second_offset) = Self::split_discriminants_to_offsets(self.discriminant());
+        let (first_offset, second_offset) = Self::split_index_to_offsets(self.index());
         Some(unsafe { Self::VALUES.as_ptr().offset(first_offset).offset(second_offset).read() })
     }
 
@@ -15,11 +15,11 @@ pub trait Valued: Indexed {
     }
 
     fn value_to_variant_opt(value: &Self::Value) -> Option<Self> {
-        let discriminant = Self::VALUES.iter()
+        let index = Self::VALUES.iter()
             .enumerate()
             .filter(|(_, variant_value)| value.eq(variant_value)).next()
             .map(|(index, _)| index);
-        Self::from_discriminant_opt(discriminant?)
+        Self::from_index_opt(index?)
     }
 
     fn value_to_variant(value: &Self::Value) -> Self {
