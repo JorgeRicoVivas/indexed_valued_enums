0b23e756730e490c3a69db3e0a99cf9a9d2e9775
Jorge <jorgericoinformatic@gmail.com>
2023-11-29 16:51:22 +0100

    Finished documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 #![no_std]
 #![no_main]
 
-//! A crate to easily create C-like enums resolving into values
+//! A crate to easily create C-like enums resolving into values.
 //!
 //! ## Example of valued enum use
-//! This creates an public enum where every Number has an associated value of type NumberDescription
+//! This creates a public enum where every Number has an associated value of type NumberDescription.
 //! ```rust
 //! use indexed_valued_enums::create_indexed_valued_enum;
 //! use indexed_valued_enums::indexed_enum::Indexed;
@@ -76,39 +76,42 @@
 //! On each of the fields you can indicate different parameters to change the implementation of the
 //! enum:
 //!
-//! * *Visibility*: Visibility of the enum
-//! * *EnumsName*: Name the enum will have
-//! * *Derives*: List of derive macros you want the enum to execute
-//! * *TypeOfValue*: type of the values the variant's resolve to
+//! * *Documentation*: Documentation of the enum.
+//! * *Derives*: List of derive macros you want the enum to execute (Optional).
+//! * *Visibility*: Visibility of the enum (Optional).
+//! * *EnumsName*: Name the enum will have.
+//! * *TypeOfValue*: type of the values the variant's resolve to.
 //! * Pairs of *Variant, Value*: Name of the variant's to create along to the name they resolve to,
-//!                              the values must be const and have 'static lifetime
+//!                              the values must be const and have 'static lifetime.
 //! * *Features*: List of specific implementations you want your enum to use, they are the following ones:
 //!     * DerefToValue: The enum implements Deref, making variants to resolve to their value
 //!                     directly, remember however these values won't mutate as they are constant
 //!                     references (&'static *TypeOfValue*), this is also the reason why these
-//!                     values require their life-time to be 'static
+//!                     values require their life-time to be 'static.
 //!     * Clone: The enum implements clone calling [Indexed::from_discriminant], this way it's not
-//!              required for the Derive Clone macro to expand to large enums
+//!              required for the Derive Clone macro to expand to large enums.
 //!     * Delegators: Implements delegator functions over this enum that call on the methods from
 //!                  [Indexed] and [Valued], this way it is not required to import or use the
 //!                  indexed_valued_enums crate directly, however, it doesn't delegate the methods
 //!                  [Valued::value_to_variant] and [Valued::value_to_variant_opt] as they
 //!                  require the type of value to implement [PartialEq], however, you can delegate
-//!                  these too with the feature **ValueToVariantDelegators**
+//!                  these too with the feature **ValueToVariantDelegators**.
 //!     * ValueToVariantDelegators: Implements delegator functions for [Valued::value_to_variant]
-//!                                 and [Valued::value_to_variant_opt]
+//!                                 and [Valued::value_to_variant_opt].
 //!     * Serialize: Implements serde's Serialize trait where it serializes to an usize that
-//!                  represents this enum's discriminant
+//!                  represents this enum's discriminant. <br>
+//!                  Requires the "serde_enums" feature.
 //!     * Deserialize: Implements serde's Deserialize trait where it deserializes an enum variant's
-//!                    from it's enum's discriminant
+//!                    from it's enum's discriminant. <br>
+//!                    Requires the "serde_enums" feature.
 //!     * NanoSerBin: Implements nanoserde's SerBin trait where it serializes to an usize that
-//!                   represents this enum's discriminant
+//!                   represents this enum's discriminant.
 //!     * NanoDeBin: Implements nanoserde's DeBin trait where it deserializes an enum variant's
-//!                  from it's enum's discriminant
+//!                  from it's enum's discriminant.
 //!     * NanoSerJson: Implements nanoserde's SerJson trait where it serializes to an usize that
-//!                   represents this enum's discriminant
+//!                   represents this enum's discriminant.
 //!     * NanoDeJson: Implements nanoserde's DeJson trait where it deserializes an enum variant's
-//!                  from it's enum's discriminant
+//!                  from it's enum's discriminant.
 
 /// Defines a trait to associate values to an enum
 pub mod valued_enum;
--- src/macros.rs
+++ src/macros.rs
@@ -5,9 +5,9 @@ use crate::valued_enum::Valued;
 /// produce unique numbers of usize to identify each variant through [Indexed::discriminant], and
 /// get back those variants through [Indexed::from_discriminant], and similar to it, each variant
 /// has a value that can be taken from [Valued::value], where the variant can be taken back* from
-/// that  value through [Valued::value_to_variant]
+/// that  value through [Valued::value_to_variant].
 ///
-/// *Just if the value isn't repeated
+/// *Just if the value isn't repeated.
 /// <br><br>
 /// To implement it write:
 /// <br><br>
@@ -49,40 +49,42 @@ use crate::valued_enum::Valued;
 /// On each of the fields you can indicate different parameters to change the implementation of the
 /// enum:
 ///
-/// * *Documentation*: Documentation of the enum
-/// * *Derives*: List of derive macros you want the enum to execute (Optional)
-/// * *Visibility*: Visibility of the enum (Optional)
-/// * *EnumsName*: Name the enum will have
-/// * *TypeOfValue*: type of the values the variant's resolve to
+/// * *Documentation*: Documentation of the enum.
+/// * *Derives*: List of derive macros you want the enum to execute (Optional).
+/// * *Visibility*: Visibility of the enum (Optional).
+/// * *EnumsName*: Name the enum will have.
+/// * *TypeOfValue*: type of the values the variant's resolve to.
 /// * Pairs of *Variant, Value*: Name of the variant's to create along to the name they resolve to,
-///                              the values must be const and have 'static lifetime
+///                              the values must be const and have 'static lifetime.
 /// * *Features*: List of specific implementations you want your enum to use, they are the following ones:
 ///     * DerefToValue: The enum implements Deref, making variants to resolve to their value
 ///                     directly, remember however these values won't mutate as they are constant
 ///                     references (&'static *TypeOfValue*), this is also the reason why these
-///                     values require their life-time to be 'static
+///                     values require their life-time to be 'static.
 ///     * Clone: The enum implements clone calling [Indexed::from_discriminant], this way it's not
-///              required for the Derive Clone macro to expand to large enums
+///              required for the Derive Clone macro to expand to large enums.
 ///     * Delegators: Implements delegator functions over this enum that call on the methods from
 ///                  [Indexed] and [Valued], this way it is not required to import or use the
 ///                  indexed_valued_enums crate directly, however, it doesn't delegate the methods
 ///                  [Valued::value_to_variant] and [Valued::value_to_variant_opt] as they
 ///                  require the type of value to implement [PartialEq], however, you can delegate
-///                  these too with the feature **ValueToVariantDelegators**
+///                  these too with the feature **ValueToVariantDelegators**.
 ///     * ValueToVariantDelegators: Implements delegator functions for [Valued::value_to_variant]
-///                                 and [Valued::value_to_variant_opt]
+///                                 and [Valued::value_to_variant_opt].
 ///     * Serialize: Implements serde's Serialize trait where it serializes to an usize that
-///                  represents this enum's discriminant
+///                  represents this enum's discriminant. <br>
+///                  Requires the "serde_enums" feature.
 ///     * Deserialize: Implements serde's Deserialize trait where it deserializes an enum variant's
-///                    from it's enum's discriminant
+///                    from it's enum's discriminant. <br>
+///                    Requires the "serde_enums" feature.
 ///     * NanoSerBin: Implements nanoserde's SerBin trait where it serializes to an usize that
-///                   represents this enum's discriminant
+///                   represents this enum's discriminant.
 ///     * NanoDeBin: Implements nanoserde's DeBin trait where it deserializes an enum variant's
-///                  from it's enum's discriminant
+///                  from it's enum's discriminant.
 ///     * NanoSerJson: Implements nanoserde's SerJson trait where it serializes to an usize that
-///                   represents this enum's discriminant
+///                   represents this enum's discriminant.
 ///     * NanoDeJson: Implements nanoserde's DeJson trait where it deserializes an enum variant's
-///                  from it's enum's discriminant
+///                  from it's enum's discriminant.
 #[macro_export]
 macro_rules! create_indexed_valued_enum {
         (
