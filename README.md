a0525ed01948e6e1fdae1e0091924e0b124fd849
Jorge <jorgericoinformatic@gmail.com>
2024-03-03 00:46:14 +0100

    Added badges to documentation.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-![crates.io](https://img.shields.io/crates/v/indexed_valued_enums.svg)
-![GitHub Actions Workflow Status](https://img.shields.io/github/actions/workflow/status/JorgeRicoVivas/indexed_valued_enums/rust.yml)
-![docs.rs](https://img.shields.io/docsrs/indexed_valued_enums)
-![GitHub License](https://img.shields.io/github/license/JorgeRicoVivas/indexed_valued_enums)
+[![crates.io](https://img.shields.io/crates/v/indexed_valued_enums.svg)](https://crates.io/crates/indexed_valued_enums)
+[![GitHub Actions Workflow Status](https://img.shields.io/github/actions/workflow/status/JorgeRicoVivas/indexed_valued_enums/rust.yml)](https://github.com/JorgeRicoVivas/indexed_valued_enums/actions)
+[![docs.rs](https://img.shields.io/docsrs/indexed_valued_enums)](https://docs.rs/indexed_valued_enums/latest/indexed_valued_enums/)
+[![GitHub License](https://img.shields.io/github/license/JorgeRicoVivas/indexed_valued_enums)](https://github.com/JorgeRicoVivas/indexed_valued_enums?tab=CC0-1.0-1-ov-file)
 
 Create enums resolving into values, and get their variants back through their values or their
 discriminant, inspired by Java's enums.
--- indexed_valued_enums/src/lib.rs
+++ indexed_valued_enums/src/lib.rs
@@ -1,10 +1,10 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 
-//! ![crates.io](https://img.shields.io/crates/v/indexed_valued_enums.svg)
-//! ![GitHub Actions Workflow Status](https://img.shields.io/github/actions/workflow/status/JorgeRicoVivas/indexed_valued_enums/rust.yml)
-//! ![docs.rs](https://img.shields.io/docsrs/indexed_valued_enums)
-//! ![GitHub License](https://img.shields.io/github/license/JorgeRicoVivas/indexed_valued_enums)
+//! [![crates.io](https://img.shields.io/crates/v/indexed_valued_enums.svg)](https://crates.io/crates/indexed_valued_enums)
+//! [![GitHub Actions Workflow Status](https://img.shields.io/github/actions/workflow/status/JorgeRicoVivas/indexed_valued_enums/rust.yml)](https://github.com/JorgeRicoVivas/indexed_valued_enums/actions)
+//! [![docs.rs](https://img.shields.io/docsrs/indexed_valued_enums)](https://docs.rs/indexed_valued_enums/latest/indexed_valued_enums/)
+//! [![GitHub License](https://img.shields.io/github/license/JorgeRicoVivas/indexed_valued_enums)](https://github.com/JorgeRicoVivas/indexed_valued_enums?tab=CC0-1.0-1-ov-file)
 //!
 //! Create enums resolving into values, and get their variants back through their values or their
 //! discriminant, inspired by Java's enums.
